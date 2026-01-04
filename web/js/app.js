9abd033f02d3e0a62c04e1bcb1a38facd51cc96b
Hubet Cardenas-Isla <hubetcardenasi@gmail.com>
2026-01-04 01:29:26 -0800

    Added actual year validation to index

--- web/js/app.js
+++ web/js/app.js
@@ -5,3 +5,17 @@ function mostrarPopup() {
 function cerrarPopup() {
     document.getElementById("popup").style.display = "none";
 }
+
+function getActualYear() {
+    const date = new Date();
+    return date.getFullYear();
+}
+
+let yearElement = document.getElementById("actualYear");
+
+if (yearElement) {
+    let yearActual = getActualYear();
+    if(yearActual > 2025){
+        yearElement.textContent = "2025 - " + getActualYear();
+    }
+}
